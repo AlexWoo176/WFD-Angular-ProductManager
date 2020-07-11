7e0d46a68ae35e44e5266ec474bc875815e8e509
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-12 00:28:15 +0700

    get list book for server

--- src/app/books/list/list.component.ts
+++ src/app/books/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book} from '../../interface/book';
+import {BookService} from '../../services/book.service';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  bookList: Book[];
 
-  constructor() { }
+  constructor(private bookService: BookService) {
+  }
 
   ngOnInit(): void {
+    this.bookService.getBook().subscribe(next => {
+      this.bookList = next;
+      console.log(this.bookList);
+    });
   }
 
 }
