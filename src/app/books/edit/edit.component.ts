9f776f5aa64cc41170260687ae0be33e964f30e5
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-12 11:46:21 +0700

    add edit

--- src/app/books/edit/edit.component.ts
+++ src/app/books/edit/edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book} from '../../interface/book';
+import {Subscription} from 'rxjs';
+import {BookService} from '../../services/book.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  message: string;
+  book: Book;
+  subcription: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private bookService: BookService,
+              private activatedRoute: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    this.subcription = this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      const id = paramMap.get('id');
+      this.bookService.bookDetail(+id).subscribe(next => {
+        this.book = next;
+      }, error => {
+        console.log(error);
+      });
+    });
+  }
+
+  edit(bookForm) {
+    console.log(this.book.id);
+    this.bookService.editBook(this.book.id, bookForm.value).subscribe(next => {
+      this.message = 'Đã sửa thành công';
+    }, error => {
+      this.message = 'Sửa không được thì bỏ';
+    });
+  }
 }
